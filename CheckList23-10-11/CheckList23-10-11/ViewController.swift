240d26d05345f9f3dd151a08ac7b24d6e36b518d
shunsuke250 <soeyamashunsuke@gmail.com>
2023-10-11 21:20:47 +0900

    :sparkles: プロジェクトを作成

---
+++ CheckList23-10-11/CheckList23-10-11/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CheckList23-10-11
+//
+//  Created by 副山俊輔 on 2023/10/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
