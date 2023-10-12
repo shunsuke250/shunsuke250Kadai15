213f1e2da45a3f44257d77e6156961a570f46e4d
shunsuke250 <soeyamashunsuke@gmail.com>
2023-10-12 22:11:38 +0900

    :recycle: 不要なメソッドを削除

--- CheckList23-10-11/CheckList23-10-11/CheckListTableViewCell.swift
+++ CheckList23-10-11/CheckList23-10-11/CheckListTableViewCell.swift
@@ -31,10 +31,4 @@ class CheckListTableViewCell: UITableViewCell {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
 }
