c174780580efa211b51fab25ad69ffb9efadeb11
shunsuke250 <soeyamashunsuke@gmail.com>
2023-10-14 22:36:47 +0900

    :sparkles: アイテムを追加する画面を作成

---
+++ CheckList23-10-11/CheckList23-10-11/AddViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddViewController.swift
+//  CheckList23-10-11
+//
+//  Created by 副山俊輔 on 2023/10/14.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
