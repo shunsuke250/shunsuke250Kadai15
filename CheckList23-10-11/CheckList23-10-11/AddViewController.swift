8de04777d923f0b8b38a6cdc43671d882970d854
shunsuke250 <soeyamashunsuke@gmail.com>
2023-10-14 22:38:12 +0900

    :sparkles: アイテムを追加する画面のUIを定義

--- CheckList23-10-11/CheckList23-10-11/AddViewController.swift
+++ CheckList23-10-11/CheckList23-10-11/AddViewController.swift
@@ -9,21 +9,59 @@ import UIKit
 
 class AddViewController: UIViewController {
 
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "名前"
+        return label
+    }()
+
+    private let addTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+
+    private var saveBarButtonItem: UIBarButtonItem!
+    private var cancelBarButtonItem: UIBarButtonItem!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        setupComponents()
+        saveBarButtonItem = UIBarButtonItem(
+            title: "Save",
+            style: .done,
+            target: self,
+            action: #selector(saveFruit)
+        )
+        cancelBarButtonItem = UIBarButtonItem(
+            title: "Cancel",
+            style: .plain,
+            target: self,
+            action: #selector(cancel)
+        )
+        navigationItem.rightBarButtonItem = saveBarButtonItem
+        navigationItem.leftBarButtonItem = cancelBarButtonItem
     }
-    
 
-    /*
-    // MARK: - Navigation
+    private func setupComponents() {
+        view.addSubview(nameLabel)
+        view.addSubview(addTextField)
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        nameLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(50)
+            $0.top.equalToSuperview().offset(150)
+        }
+        addTextField.snp.makeConstraints {
+            $0.left.equalTo(nameLabel.snp.right).offset(20)
+            $0.centerY.equalTo(nameLabel.snp.centerY)
+            $0.width.equalTo(200)
+        }
     }
-    */
 
+    @objc func saveFruit() {
+    }
+
+    @objc func cancel() {
+    }
 }
